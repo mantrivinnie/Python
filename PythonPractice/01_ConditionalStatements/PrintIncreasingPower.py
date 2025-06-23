9c2648ae2494c4a5daca5aa98abf5af9e38225f3
mantrivinnie <mantrivinnie@gmail.com>
2025-06-23 19:56:15 +0530

    Add file print increasing power number

---
+++ PythonPractice/01_ConditionalStatements/PrintIncreasingPower.py
@@ -0,0 +1,10 @@
+#Given a positive integer x, the task is to print the numbers from 1 to x in the order as 12, 22, 32, 42, 52, ... (in increasing order).
+
+# Function to print x in increasing order
+def printIncreasingPower(x):
+    ##Your code here
+    i=1
+    while(i*i<=x):
+        print (i*i , end = " ");
+        i=i+1;
+        
