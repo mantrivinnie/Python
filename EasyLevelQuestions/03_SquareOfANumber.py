2c6f79360fc39ab9ee7faa1ff5aff43b639223c5
mantrivinnie <mantrivinnie@gmail.com>
2025-08-25 22:30:43 +0530

    Add file to square a number

---
+++ EasyLevelQuestions/03_SquareOfANumber.py
@@ -0,0 +1,21 @@
+# This program gives the squared value of the input provided by the user.
+
+def SquareNum(x):
+    return x*x
+try:
+
+    # Get input from the user.
+    # The input() function returns a string, so we need to convert it to a float
+    # to perform mathematical operations.
+    x=float(input("Enter the number:\n"))
+
+    # Call the function to get the sum.
+    square = SquareNum(x)
+
+    # Display the result to the user.
+    # The f-string formats the output nicely.
+    print(f"The square of {x} is {square}")
+
+except ValueError:
+    # Handle cases where the user enters non-numeric input.
+    print("Invalid input. Please enter a valid number.")
