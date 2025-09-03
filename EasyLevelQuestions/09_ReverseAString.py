26bc9de3f34248df3e6f76a72df7bc453f8b62c6
mantrivinnie <mantrivinnie@gmail.com>
2025-09-03 22:08:43 +0530

    Add more code

--- EasyLevelQuestions/09_ReverseAString.py
+++ EasyLevelQuestions/09_ReverseAString.py
@@ -12,3 +12,9 @@ for char in text:
     reversed_text = char + reversed_text   # prepend each character
 
 print("Reversed string:", reversed_text)
+
+
+# Reverse a string using reversed()
+text = input("Enter a string: ")
+reversed_text = "".join(reversed(text))
+print("Reversed string:", reversed_text)
