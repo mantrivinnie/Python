a06b025e0ce386b328115d3447acfbbe20935822
Vinisha Mantri <mantrivinnie@gmail.com>
2025-09-21 23:38:46 +0530

    Add file 16_CountFreqOfWordsInString

---
+++ ModerateLevelQuestions/16_CountFreqOfWordsInString.py
@@ -0,0 +1,25 @@
+def word_frequency(text):
+    # Convert to lowercase and split into words
+    words = text.lower().split()
+    freq = {}
+    
+    for word in words:
+        freq[word] = freq.get(word, 0) + 1   # count occurrences
+    
+    return freq
+
+# Take input
+text = input("Enter a string: ")
+result = word_frequency(text)
+
+print("Word frequency:")
+for word, count in result.items():
+    print(f"{word}: {count}")
+
+#Enter a string: This is a test This is simple
+#Word frequency:
+#this: 2
+#is: 2
+#a: 1
+#test: 1
+#simple: 1
