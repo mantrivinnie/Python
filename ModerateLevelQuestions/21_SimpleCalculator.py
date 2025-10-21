f09bd526997f3df83001d553283f6518db852379
mantrivinnie <mantrivinnie@gmail.com>
2025-10-21 19:53:51 +0530

    Update file to implement simple calculator

--- ModerateLevelQuestions/21_SimpleCalculator.py
+++ ModerateLevelQuestions/21_SimpleCalculator.py
@@ -1,3 +1,6 @@
+#program to implement a basic calculator that performs +, -, *, /
+
+
 def calculator(a, b, operator):
     if operator == '+':
         return a + b
@@ -23,3 +26,16 @@ try:
 
 except ValueError:
     print("Invalid input! Please enter numeric values.")
+
+#Example 1
+#Enter first number: 10
+#Enter second number: 5
+#Enter operator (+, -, *, /): *
+#Result: 50.0
+
+#Example 2
+#Enter first number: 8
+#Enter second number: 0
+#Enter operator (+, -, *, /): /
+#Result: Error! Division by zero.
+
