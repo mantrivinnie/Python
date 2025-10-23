5922b52bdb7b3f3d48edc9b4a05eb4fd54503137
mantrivinnie <mantrivinnie@gmail.com>
2025-10-23 18:47:31 +0530

    Add file to find GCD of two numbers

--- ModerateLevelQuestions/24_GCDOfTwoNo.py
+++ ModerateLevelQuestions/24_GCDOfTwoNo.py
@@ -15,3 +15,10 @@ print(f"GCD of {num1} and {num2} is {gcd(num1, num2)}")
 #Enter first number: 48
 #Enter second number: 18
 #GCD of 48 and 18 is 6
+
+#Alternate Method 
+import math
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("GCD is:", math.gcd(num1, num2))
+
