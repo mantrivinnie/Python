51a95c1d92e93bea078bad7ed7ebee483b3c4800
mantrivinnie <mantrivinnie@gmail.com>
2025-10-24 20:16:50 +0530

    Update file for transpose of a matrix

--- ModerateLevelQuestions/25_TransposeOfMatrix.py
+++ ModerateLevelQuestions/25_TransposeOfMatrix.py
@@ -31,3 +31,9 @@ for row in matrix:
 print("\nTranspose:")
 for row in result:
     print(row)
+
+
+#Alternate Method 
+matrix = [[1, 2, 3], [4, 5, 6]]
+transpose = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+print(transpose)
