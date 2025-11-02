984662c5bab3b42f7ac57c833bba3ae1539184ee
mantrivinnie <mantrivinnie@gmail.com>
2025-11-02 22:26:54 +0530

    update file for bubble sort

--- HardLevelQuestions/27_BubbleSort.py
+++ HardLevelQuestions/27_BubbleSort.py
@@ -23,3 +23,11 @@ print("Sorted array:", sorted_arr)
 #Example
 #Original array: [64, 34, 25, 12, 22, 11, 90]
 #Sorted array: [11, 12, 22, 25, 34, 64, 90]
+
+
+#Explanation:
+#The algorithm repeatedly compares adjacent elements and swaps them if they’re in the wrong order.
+#After each outer loop, the largest element moves (“bubbles up”) to its correct position.
+
+#Time Complexity: O(n²)
+#Best Case (sorted array): O(n)
