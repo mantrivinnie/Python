98171b78c33f1bfb3e9277b27054fbb3b18eeeee
mantrivinnie <mantrivinnie@gmail.com>
2026-01-23 00:43:24 +0530

    04_file add

---
+++ VerilogProjectWorks/Real_To_FP16_Conversion/04_Image_to_Pixel_to_FP16.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+
+img = Image.open("smile1.jpeg").convert("L")
+img = img.resize((100, 100))
+
+pixels = np.array(img)
+
+with open("image_fp16.mem", "w") as f:
+    for y in range(100):
+        for x in range(100):
+            val = float(pixels[y, x])
+            fp16 = np.float16(val)
+            hex_val = fp16.view(np.uint16)
+            f.write(f"{hex_val:04X}\n")
+
+print("image_fp16.mem generated")
