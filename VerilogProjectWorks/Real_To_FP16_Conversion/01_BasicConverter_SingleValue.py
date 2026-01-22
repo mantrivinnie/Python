cedcb3c0a3e8ce7efdbbbe29b4a652e64efe6060
mantrivinnie <mantrivinnie@gmail.com>
2026-01-23 00:24:10 +0530

    Basic converter single input value

---
+++ VerilogProjectWorks/Real_To_FP16_Conversion/01_BasicConverter_SingleValue.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def float_to_fp16_hex(value):
+    fp16 = np.float16(value)
+    hex_val = fp16.view(np.uint16)
+    return f"0x{hex_val:04X}"
+
+# Examples
+print(float_to_fp16_hex(1.0))     # 0x3C00
+print(float_to_fp16_hex(0.5))     # 0x3800
+print(float_to_fp16_hex(1.25))    # 0x3D00
+print(float_to_fp16_hex(-0.5))    # 0xB800
+print(float_to_fp16_hex(3.25))    # 0x4280
